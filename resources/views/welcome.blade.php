26db4c0188fe3ebdcbc32e50329b7ad1803b3e0d
flaviodematoshenrique <digitalclasse@gmail.com>
2023-01-11 13:05:29 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Templates</title>
+</head>
+<body>
+    <h1>Meu primeiro template Blade</h1>
+</body>
+</html>
